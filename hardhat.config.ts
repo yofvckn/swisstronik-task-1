94429fa4e1543f72f554335e622edf5fae0fe9db
yofvckn <yofan.soepandi18@gmail.com>
2024-08-12 02:32:21 +0700

    new commit to swisstronik

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,7 @@ const config: HardhatUserConfig = {
   networks: {
     swisstronik: {
       url: 'https://json-rpc.testnet.swisstronik.com/', //URL of the RPC node for Swisstronik.
-      accounts: [`0x${process.env.PRIVATE_KEY}`], //Your private key starting with "0x"
+      accounts: [`0xc3ffae057f9df3ec2246dbedd4f401957171fdb1915ff1f871308cf5560cc286`], //Your private key starting with "0x"
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,3 +1,3 @@
-const deployedAddress = ''
+const deployedAddress = '0xf641e81A9F52E32DAd40E66ED56D730de9424b04'
 
 export default deployedAddress
